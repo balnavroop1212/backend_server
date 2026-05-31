770750095998f08194299c652bbdb83a0410e087
balnavroop1212 <balb8781@gmail.com>
2026-05-31 15:10:44 +0530

    updated system of complaint 1

--- index.js
+++ index.js
@@ -275,4 +275,4 @@ app.post('/api/complaints/update-status', async (req, res) => {
 
 // 5. Start Server
 const PORT = process.env.PORT || 5000; 
-app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));�� Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
